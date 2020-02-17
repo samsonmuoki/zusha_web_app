1d1082eeafeedf0bbdefea3d210a16c0750fdc36
sam <smuoki@healthcloud.co.ke>
2020-02-17 11:47:45 +0300

    Add license status

--- zusha/registration/models.py
+++ zusha/registration/models.py
@@ -2,20 +2,42 @@ from django.db import models
 
 # Create your models here.
 
+APPROVED = 'approved'
+BLACKLISTED = 'blacklisted'
+
+STATUS = [
+    (APPROVED, ('Approved to operate')),
+    (BLACKLISTED, ('Not approved to operate')),
+]
+
 
 class Sacco(models.Model):
     """Sacco details."""
 
     sacco_name = models.CharField(max_length=200)
+    # date registered = models.DateField
+    # last_report_revision_date = models.DateField
+    license_status = models.CharField(
+        max_length=32,
+        choices=STATUS,
+        default=APPROVED,
+    )
 
     def __str__(self):
-        return self.sacco_name
+        return f"{self.sacco_name} => {self.license_status}"
 
 
 class Vehicle(models.Model):
     """Vehicle details."""
     registration_number = models.CharField(max_length=200)
     sacco = models.ForeignKey(Sacco, on_delete=models.PROTECT)
+    # date registered = models.DateField
+    # last_report_revision_date = models.DateField
+    license_status = models.CharField(
+        max_length=32,
+        choices=STATUS,
+        default=APPROVED,
+    )
 
     def __str__(self):
         return f"{self.registration_number} => {self.sacco}"
@@ -28,6 +50,13 @@ class Driver(models.Model):
     driver_id = models.CharField(max_length=200)
     # driver_id = models.IntegerField(default=0)
     sacco = models.ForeignKey(Sacco, on_delete=models.PROTECT)
+    # date registered = models.DateField
+    # last_report_revision_date = models.DateField
+    license_status = models.CharField(
+        max_length=32,
+        choices=STATUS,
+        default=APPROVED,
+    )
 
     def __str__(self):
         return f"{self.driver_name} => {self.sacco}"
