79110644c689ff4938b1f9489c8598f3f5978795
samsonmuoki <samsonmuoki97@gmail.com>
2020-05-10 21:47:49 +0300

    Update playbooks

--- ansible_playbooks/fabfile.py
+++ ansible_playbooks/fabfile.py
@@ -6,3 +6,4 @@ def deploy():
     local('ansible-playbook nginx_playbook.yml -i hosts.yml')
     local('ansible-playbook gunicorn_playbook.yml -i hosts.yml')
     local('ansible-playbook letsencrypt_playbook.yml -i hosts.yml')
+    local('ansible-playbook periodic_tasks_playbook.yml -i hosts.yml')
