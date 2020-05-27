4e1fcecc4fcb5f234236c060ec727359a92c4167
samsonmuoki <samsonmuoki97@gmail.com>
2020-05-27 21:02:09 +0300

    Add cd task

--- ansible_playbooks/gcp/fabfile.py
+++ ansible_playbooks/gcp/fabfile.py
@@ -6,4 +6,11 @@ def deploy():
     local('ansible-playbook nginx_playbook.yml -i hosts.yml')
     local('ansible-playbook gunicorn_playbook.yml -i hosts.yml')
     local('ansible-playbook letsencrypt_playbook.yml -i hosts.yml')
-    # local('ansible-playbook periodic_tasks_playbook.yml -i hosts.yml')
+
+
+def ci_cd():
+    local('ansible-playbook cd_playbook.yml -i hosts.yml')
+
+
+def periodic_tasks():
+    local('ansible-playbook periodic_tasks_playbook.yml -i hosts.yml')
